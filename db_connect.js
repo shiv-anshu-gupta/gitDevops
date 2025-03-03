71d220287c6b01999e33c6da236a3fff8117e0c4
unknown <shivanshu2019gupta@gmail.com>
2025-03-03 15:41:11 +0530

    second update

---
+++ db_connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/myapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
